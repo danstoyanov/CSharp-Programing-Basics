c8baccc15aa640af076c6d6feb9e1845c94215c1
danstoyanov <dan.stoyanov@abv.bg>
2021-01-14 17:28:54 +0200

    update

---
+++ Loops/Loops_Lab/08_Number_sequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08_Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            for (int i = 1; i <= input; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    } 
+}  
