65a7c1a75f494710749907e378692fbe77817544
danstoyanov <dan.stoyanov@abv.bg>
2021-01-14 17:27:14 +0200

    update

---
+++ Loops/Loops_Lab/06_Vowels_Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06_Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int counter = 0; counter < input.Length; counter++)
+            { 
+                if (input[counter] == 'a')
+                {
+                    sum += 1;
+                }
+                if (input[counter] == 'e')
+                {
+                    sum += 2;
+                }
+                if (input[counter] == 'i')
+                {
+                    sum += 3;
+                }
+                if (input[counter] == 'o')
+                {
+                    sum += 4;
+                }
+                if (input[counter] == 'u')
+                {
+                    sum += 5;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+} 
