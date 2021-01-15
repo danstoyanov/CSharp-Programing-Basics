b5c3793ec6632bb60b404d9b26c4ed0814722cb3
danstoyanov <dan.stoyanov@abv.bg>
2021-01-15 10:51:38 +0200

    update

---
+++ Loops/Loops_Exercise/03_Odd_or_Even_Position/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_Odd_or_Even_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
