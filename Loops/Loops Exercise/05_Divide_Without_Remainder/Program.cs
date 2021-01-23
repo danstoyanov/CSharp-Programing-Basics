4db2e85aa1eaf4652bbab678fdf691cb3b184440
danstoyanov <dan.stoyanov@abv.bg>
2021-01-23 13:44:32 +0200

    update

---
+++ Loops/Loops Exercise/05_Divide_Without_Remainder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05_Divide_Without_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
