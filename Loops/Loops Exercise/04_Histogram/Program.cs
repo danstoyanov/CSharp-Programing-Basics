d03c01dbd9032b1d7bb2febfd891cbf8a064347e
danstoyanov <dan.stoyanov@abv.bg>
2021-01-23 13:44:21 +0200

    update

---
+++ Loops/Loops Exercise/04_Histogram/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _04_Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int counterGroupOne = 0;
+            int counterGroupTwo = 0;
+            int counterGroupThree = 0;
+            int counterGroupFour = 0;
+            int counterGroupFive = 0;
+
+            for (int number = 1; number <= n; number++)
+            {
+                int value = int.Parse(Console.ReadLine());
+
+                if (value < 200)
+                {
+                    counterGroupOne++;
+                }
+
+                else if (value >= 200 && value <= 399)
+                {
+                    counterGroupTwo++;
+                }
+
+                else if (value >= 400 && value <= 599)
+                {
+                    counterGroupThree++;
+                }
+                else if (value >= 600 && value <= 799)
+                {
+                    counterGroupFour++;
+                }
+                else
+                {
+                    counterGroupFive++;
+                }
+            }
+
+            double p1 = counterGroupOne * 1.0 / n * 100;
+            double p2 = counterGroupTwo * 1.0 / n * 100;
+            double p3 = counterGroupThree * 1.0 / n * 100;
+            double p4 = counterGroupFour * 1.0 / n * 100;
+            double p5 = counterGroupFive * 1.0 / n * 100;
+
+            Console.WriteLine($"{p1:F2}%");
+            Console.WriteLine($"{p2:F2}%");
+            Console.WriteLine($"{p3:F2}%");
+            Console.WriteLine($"{p4:F2}%");
+            Console.WriteLine($"{p5:F2}%");
+        }
+    }
+}
